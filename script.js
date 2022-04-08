efc5081d69eb0b00a4e51789f41fc2c96042213e
usman <muhammad.usman@brainxtech.com>
2022-04-08 13:06:40 +0500

    updated commit

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
 
 function enableSubmit(){
-    const fname = document.getElementById('fname');
-    const lname = document.getElementById('lname');
-    const age = document.getElementById('age');
-    const notify = document.getElementById('notify-email');
+    const fname = document.getElementById('fname').value;
+    if(fname.toString().length == 0){
+        return false;
+    }
+    const lname = document.getElementById('lname').value;
+    if(lname.toString().length == 0){
+        return false;
+    }
+    const age = document.getElementById('age').value;
+    if(age.toString().length == 0){
+        return false;
+    }
+    const notify = document.getElementById('notify-email').value;
+    if(notify.toString().length == 0){
+        return false;
+    }
+    if(!validateAge()){
+        document.getElementById("submit-button").setAttribute("disabled","true");
+        return false;
+    }
     if(!checkElevenDigit()){
         document.getElementById("submit-button").setAttribute("disabled","true");
         return false;
@@ -21,6 +37,22 @@ function enableSubmit(){
     }
 }
 
+function validateAge(){
+    const age = document.getElementById('age').value;
+    if(age == ''){
+        return false;
+    }
+    else if(age < 18 || age > 150){
+        document.getElementById('age').style.backgroundColor = 'rgb(247,181,181)';
+        document.getElementById('age-warn').innerText = 'Age must be >= 18 and <=150';
+        return false;
+    }
+    else{
+        document.getElementById('age').style.backgroundColor = 'rgb(195, 236, 168)';
+        document.getElementById('age-warn').innerText = '';
+        return true;
+    }
+}
 
 function validatePassword(pwd1){
     if(pwd1.length < 8){
@@ -37,7 +69,10 @@ function validatePassword(pwd1){
 
 function addingPassword(){
     const pswd = document.getElementById('pwd').value;
-    if(!validatePassword(pswd)){
+    if(pswd.toString().length == 0){
+        return false;
+    }
+    else if(!validatePassword(pswd)){
         document.getElementById('pwd').style.backgroundColor = 'rgb(247,181,181)';
         document.getElementById('pwd-notifi').innerHTML = 'Password Requirements: 1 numeric & 1 lowercase & uppercase alphabet at least. Min Length = 8'
         document.getElementById('pwd-notifi').style.color ='red';
@@ -53,18 +88,26 @@ function addingPassword(){
 
 function checkElevenDigit(){
     const num = document.getElementById('contact').value;
-    if (num.toString().length == 11){
+    if(num.toString().length == 0){
+        return false;
+    }
+    else if (num.toString().length == 11){
+        document.getElementById('contact').style.backgroundColor = 'rgb(195, 236, 168)';
+        document.getElementById('contact-warn').innerText = '';
         return true;
     }
     else{
+        document.getElementById('contact').style.backgroundColor = 'rgb(247,181,181)';
+        document.getElementById('contact-warn').innerText = 'Must contain only 11 digits';
         return false;
     }
-
-
 }
 
 function validateCSEmails(){
     const text = document.getElementById('notify-email').value;
+    if(text.toString().length == 0){
+        return false;
+    }
     const emails = text.split(',');
     var valid = false;
     for (let i of emails){
@@ -87,6 +130,9 @@ function validEmail(){
     if(!/^[\s\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)){
         document.getElementById('email').style.backgroundColor = 'rgb(247,181,181)';
     }
+    else{
+        document.getElementById('email').style.backgroundColor = 'rgb(195, 236, 168)';
+    }
 }
 
 
@@ -98,7 +144,6 @@ function comparePassword(){
         document.getElementById('pwdconfirm-notifi').innerHTML = "Both password doesn't match";
         document.getElementById('pwdconfirm-notifi').style.color ='red';
         return false;
-        //alert('notequal')
     }
     else{
         document.getElementById('c-pwd').style.backgroundColor = 'rgb(195, 236, 168)';
@@ -107,10 +152,4 @@ function comparePassword(){
         return true;
         
     }
-}
-
-function validateForm(){
-    const pwd1 = document.getElementById('pwd').value;
-    const pwd2 = document.getElementById('pwd').value;
-
 }
